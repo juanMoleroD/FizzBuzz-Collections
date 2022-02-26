1660f5a250c92ba1ca1fbb372d28e223eb2ca5ca
juanMoleroD <juanldimeco@gmail.com>
2022-02-26 15:21:18 +0000

    Removed unused imports

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,11 +1,8 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 
 class ApplicationTest {
 
