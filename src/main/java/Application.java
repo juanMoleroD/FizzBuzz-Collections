ac1f6eaf2ff3e7b6ca1a137b4607167edacc951d
juanMoleroD <juanldimeco@gmail.com>
2022-02-26 15:20:14 +0000

    Streams logic implemented using the IntStream interface, which allow to make a closed rage of ints, and using mapToObj to apply the app logic, ending with a List<Strings> that we can get a value to confirm logic is correct.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,14 +1,23 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Application {
 
 
-    public List<Object> fizzbuzz(int countLimit) {
-        List<Object> result = new ArrayList<>();
 
+    public List<String> fizzbuzz(int countLimit) {
 
+        return IntStream.rangeClosed(1, countLimit)
+                .mapToObj(this::fizzBuzzMappingLogic)
+                .collect(Collectors.toList());
+    }
 
-        return result;
+    private String fizzBuzzMappingLogic(int number) {
+        if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
+        if (number % 3 == 0) return "Fizz";
+        if (number % 5 == 0) return "Buzz";
+        return Integer.toString(number);
+
     }
 }
--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,21 +1,46 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class ApplicationTest {
 
     Application app = new Application();
+    List<String> testList = app.fizzbuzz(100);
 
     @Test
     void fizzOn3() {
-        assertEquals(List.of(1,2,"Fizz"), app.fizzbuzz(3));
+        assertEquals("Fizz", testList.get(5));
+        assertEquals("Fizz", testList.get(8));
+        assertEquals("Fizz", testList.get(11));
+        assertEquals("Fizz", testList.get(32));
+        assertEquals("Fizz", testList.get(65));
+        assertEquals("Fizz", testList.get(83));
+        assertEquals("Fizz", testList.get(98));
     }
 
     @Test
     void BuzzOn5() {
-        assertEquals(List.of(1,2,"Fizz",4,"Buzz"), app.fizzbuzz(5));
+        assertEquals("Buzz", testList.get(4));
+        assertEquals("Buzz", testList.get(9));
+        assertEquals("Buzz", testList.get(19));
+        assertEquals("Buzz", testList.get(24));
+        assertEquals("Buzz", testList.get(49));
+        assertEquals("Buzz", testList.get(84));
+        assertEquals("Buzz", testList.get(99));
+    }
+
+    @Test
+    void FizzBuzzOnMultiplesOf3And5() {
+        assertEquals("FizzBuzz", testList.get(14));
+        assertEquals("FizzBuzz", testList.get(29));
+        assertEquals("FizzBuzz", testList.get(44));
+        assertEquals("FizzBuzz", testList.get(59));
+        assertEquals("FizzBuzz", testList.get(74));
+        assertEquals("FizzBuzz", testList.get(89));
     }
 }
